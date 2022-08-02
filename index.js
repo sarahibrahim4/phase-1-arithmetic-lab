83329ecc3b8b078d9660b2e735537dfdfcdab487
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-08-02 21:13:30 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10)+1;
+console.log(random);
+
+let num3 = 4;
+let num4 = 9;
+let mod= num3%num4;
+console.log(mod);
+
+let max=Math.max(3,4,6,7,20);
+console.log(max);
